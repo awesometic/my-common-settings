5995a2aab07c680c7f7afac208bd784ff0a5c7ab
Yang Deokgyu <secugyu@gmail.com>
2019-09-24 16:24:51 +0900

    Refactor codebase
    Signed-off-by: Yang Deokgyu <secugyu@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # My Common Settings
 A library for personal favorite settings to set my linux/macOS devices up more easily.<br>
-Base directory for this settings is the home directory of the user who would use this library.<br>
+Base directory for this settings is the home directory of the user who would like to use this library.<br>
+
+This script is designed for Debian/Ubuntu, Elementary OS, macOS.
 
 ## Usage
 1. Clone the repository.
@@ -8,7 +10,8 @@ Base directory for this settings is the home directory of the user who would use
 git clone --recursive https://github.com/awesometic/my_common_settings.git && cd my_common_settings
 ```
 
-2. And install preferences by using the `install.sh` script.
+2. And install preferences by using the `install.sh` script. Keep in mind that you should not execute as root.
 ```bash
-sudo source ./install.sh
+./install.sh
 ```
+
--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ ! "$OSTYPE" == *"linux-gnu"* ]]; then
-    echo 'You cannot install to non-Linux operating system.'
-    exit
-fi
+TEXT_BOLD=$(tput bold)
+TEXT_RESET=$(tput sgr0)
 
-TEXT_BOLD=`tput bold`
-TEXT_RESET=`tput sgr0`
-
-echo_msg() {
+msg() {
     echo -e "${TEXT_BOLD}"
     echo -e "/**"
     echo -e "/* $1"
@@ -16,54 +11,75 @@ echo_msg() {
     echo -e "${TEXT_RESET}"
 }
 
-echo_msg "Install useful packages..."
-sudo apt install neovim terminator tmux git htop iotop iftop gnome-tweaks shellcheck \
-    curl wget zsh build-essential python3 python3-dev python3-pip python3-setuptools imwheel
-
-echo_msg "Install shell preference using 'Oh My Zsh'..."
-export RUNZSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp -f zsh/.zshrc ~/
-
-echo_msg "Install neovim preference..."
-if [ ! -d ~/.config/nvim ]; then
-    mkdir ~/.config/nvim
+if [ "$EUID" -eq 0 ]; then
+    msg "Please do not run as root"
+    exit 0
 fi
-cp -rL --remove-destination nvim/* ~/.config/nvim/
 
-echo_msg "Install vim-plug..."
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ "$OSTYPE" == *"linux-gnu"* ]]; then
+    PACKAGES="zsh neovim tmux git htop iotop iftop inxi neofetch shellcheck curl wget build-essential python3 python3-dev python3-pip python3-setuptools"
 
-echo_msg "Execute PlugInstall command to install plugins..."
-nvim +PlugInstall +qall
-
-echo_msg "Install terminator preference..."
-if [ ! -d ~/.config/terminator ]; then
-    mkdir ~/.config/terminator
+    DESKTOP=$(env | grep XDG_CURRENT_DESKTOP)
+    [[ "${DESKTOP,,}" == *"pantheon" ]] && PACKAGES="$PACKAGES gnome-tweaks uim"
+elif [[ "$OSTYPE" == *"darwin"* ]]; then
+        PACKAGES="coreutils zsh neovim tmux git htop shellcheck python3 reattach-to-user-namespace"
+else
+    msg "System is not recognized. The program will be closed."
+    exit 0
 fi
-cp terminator/* ~/.config/terminator/config
-gsettings set org.gnome.desktop.default-applications.terminal exec '/usr/bin/terminator'
 
-echo_msg "Install mouse sensitivity autostart entry..."
-cp -f mouse/mouse.desktop ~/.config/autostart/
+msg "Install useful packages..."
 
-echo_msg "Install imwheel configuration to optimize scrolling speed and to enable side buttons..."
-cp -f imwheel/.imwheelrc ~/
-cp -f imwheel/imwheel.desktop ~/.config/autostart/
-imwheel --kill
+if [[ "$OSTYPE" == *"linux-gnu"* ]]; then
+    echo "$PACKAGES" | xargs sudo apt install -y
+else
+    echo "$PACKAGES" | xargs brew install -y
+fi
 
-echo_msg "Optimize docker to enable minimize click action..."
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+if [[ "$PACKAGES" == *"zsh"* ]]; then
+    msg "Install shell preference using 'Oh My Zsh'..."
+    export RUNZSH=no
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/"$USER"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions /home/"$USER"/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    cp -f zsh/.zshrc ~/
+fi
 
-echo_msg "Configure global git configuration..."
+if [[ "$PACKAGES" == *"neovim"* ]]; then
+    msg "Install neovim preference..."
+    if [ ! -d ~/.config/nvim ]; then
+        mkdir ~/.config/nvim
+    fi
+    cp -rL --remove-destination nvim/* ~/.config/nvim/
+
+    msg "Install vim-plug..."
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    msg "Execute PlugInstall command to install plugins..."
+    nvim +PlugInstall +qall
+fi
+
+if [[ "$PACKAGES" == *"tmux"* ]]; then
+    [[ -n $(command -v nvim) ]] && export EDITOR=nvim
+
+    msg "Install tmux preference using 'Oh My Tmux'..."
+    git clone https://github.com/gpakosz/.tmux.git /home/"$USER"/.oh-my-tmux/
+    ln -s -f /home/"$USER"/.oh-my-tmux/.tmux.conf /home/"$USER"/
+    cp /home/"$USER"/.oh-my-tmux/.tmux.conf.local /home/"$USER"/
+fi
+
+if [[ "$PACKAGES" == *"uim"* ]]; then
+    msg "Configure input method to use 'uim'..."
+    im-config -n uim
+fi
+
+msg "Configure global git configuration..."
 git config --global user.name "Yang Deokgyu"
 git config --global user.email "secugyu@gmail.com"
 git config --global user.username "awesometic"
 git config --global core.editor "nvim"
 git config --global color.ui "auto"
 
-echo_msg "You should set your system up using gnome-tweaks to your liking."
-echo_msg "Done! Reboot your system to apply changes."
+msg "You should set your system up to your liking for finishing."
+msg "Done! Reboot your system to apply changes."
 
